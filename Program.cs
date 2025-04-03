098abb11cccc9f560061e4258cccf4043daaec43
Shrijan Paudel <102872515+SrijanPoudel@users.noreply.github.com>
2025-04-03 12:47:43 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Two_Sum
+{
+    public  class Program
+    {
+        public static void Main(string[] args)
+        {
+            Twosum summ = new Twosum();
+                Console.WriteLine(string.Join(", ", summ.Sum(new int[] { 2, 7, 11, 15 }, 9)));
+
+            
+            
+        }
+    }
+}
---
+++ twosum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Two_Sum
+{
+    public class Twosum
+    {
+        public int[] Sum(int[] listt, int target)
+        {
+            int[] result = new int[2];
+
+            for (int i = 0; i < listt.Length; i++)
+            {
+                for (int j =i+ 1; j < listt.Length; j++)
+                {
+                    if (listt[i] + listt[j] == target)
+                    {
+                        result[0] = i;
+                        result[1] = j;
+
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
