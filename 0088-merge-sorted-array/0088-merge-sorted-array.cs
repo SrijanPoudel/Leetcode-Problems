6c8350509050c11df7e17503d3aade6c48989823
Shrijan Paudel <102872515+SrijanPoudel@users.noreply.github.com>
2025-04-03 12:44:23 -0500

    Time: 10 ms (4.88%), Space: 48 MB (5.71%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cs
@@ -0,0 +1,32 @@
+using System;
+
+public class Solution
+{
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        
+        for (int i = 0; i < n; i++)
+        {
+            nums1[m + i] = nums2[i];
+        }
+
+        
+        for (int i = 0; i < m + n; i++)
+        {
+            for (int j = i + 1; j < m + n; j++)
+            {
+                if (nums1[i] > nums1[j])
+                {
+                    int temp = nums1[i];
+                    nums1[i] = nums1[j];
+                    nums1[j] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < m + n; i++)
+        {
+            Console.Write(nums1[i] + " ");
+        }
+    }
+}
