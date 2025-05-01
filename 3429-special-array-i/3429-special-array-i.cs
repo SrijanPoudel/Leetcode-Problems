1782f08b46a325cd943f78b5357beb1891bf078c
Shrijan Paudel <102872515+SrijanPoudel@users.noreply.github.com>
2025-05-01 00:29:25 -0500

    Time: 0 ms (100%), Space: 46.6 MB (15.6%) - LeetHub

---
+++ 3429-special-array-i/3429-special-array-i.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public bool IsArraySpecial(int[] nums) {
+        if(nums.Length ==1){
+            return true;
+        }
+        else{
+            for(int i=0;i<nums.Length-1;i++){
+                if(nums[i]%2 == nums[i+1]%2){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+    
