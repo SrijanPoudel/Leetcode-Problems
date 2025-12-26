5276e774b9432d024d72eeeb15a7a5c8c604421e
Shrijan Paudel <102872515+SrijanPoudel@users.noreply.github.com>
2025-12-26 15:55:52 -0600

    Time: 19 ms (59.22%), Space: 25.7 MB (76.79%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        seen ={}
+        for i,num in enumerate(nums):
+            if num in seen:
+                return True
+            seen[num]=i
+        return False
+        
